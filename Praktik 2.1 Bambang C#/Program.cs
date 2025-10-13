e79fbc2df37f82a6a778960be016d2c2cebd6f76
bambang004 <jixxji4689@gmail.com>
2025-10-13 12:57:30 +0700

    Add files via upload

---
+++ Praktik 2.1 Bambang C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_2._1_Bambang_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int umur = 15;
+            String nama = "bambang";
+
+            Console.WriteLine("nama: " + nama);
+            Console.WriteLine("Umur: " + umur);
+        }
+    }
+}
---
+++ Praktik 2.1 Bambang C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Praktik 2.1 Bambang C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praktik 2.1 Bambang C#")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3101ad4e-3154-4643-8779-b7ccd0832742")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
