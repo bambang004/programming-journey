e4bb63d95a27bbc31140a970a6bbc4f553da1c45
bambang004 <jixxji4689@gmail.com>
2025-10-13 12:58:51 +0700

    Add files via upload

---
+++ Praktik 2.2 Bambang C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_2._2_Bambang_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double Tinggi = 170.5;
+            char jk = 'L';
+
+            Console.WriteLine("Tinggi badan: " + Tinggi);
+            Console.WriteLine("jk : " + jk);
+        }
+    }
+}
